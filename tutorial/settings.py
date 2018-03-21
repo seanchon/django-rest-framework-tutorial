5d1c4d9fe1acfe459f2e2ec542f32d349962fa13
Sean Chon <sean.chon@tvrpllc.com>
2018-03-21 14:10:27 -0700

    Add token authentication.

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -136,8 +136,15 @@ LOGOUT_URL = 'rest_framework:logout'
 # DRF Settings
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )}
 
-# Enable registration in django-rest-auth 
+# Enable registration in django-rest-auth
 SITE_ID = 1
